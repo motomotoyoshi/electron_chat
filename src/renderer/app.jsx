96f3239ac8fa5b7d2c630c5b6b8ec827a4a19210
motomotoyoshi <motomoto.git@gmail.com>
2019-12-24 02:25:35 +0900

    Add ,renderer

--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Route, hashHistory } from "react-router";
+import Login from "./Login";
+import Signup from "./Signup";
+import Rooms from "./Rooms";
+import Room from "./Room";
+
+const appRouting = (
+  <Router history={hashHistory}>
+    <Route path="/">
+      <Route path="login" component={Login} />
+      <Route path="signup" component={Signup} />
+      <Route path="rooms" component={Rooms}>
+        <Route path=":roomId" component={Room} />
+      </Route>
+    </Route>
+  </Router>
+);
+
+// const appRouting = <div>aaa</div>
+
+if (!location.hash.length) {
+  location.hash = "#/login";
+}
 
 render(
-  <div>Hello, Electron.</div>,
+  appRouting,
   document.getElementById("app")
 );
