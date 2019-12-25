65c339ae81285eb1f8aedc925ef552da861992ca
motomotoyoshi <motomoto.git@gmail.com>
2019-12-26 04:31:55 +0900

    Add RoomItem

--- .tmp/renderer/Rooms.js
+++ .tmp/renderer/Rooms.js
@@ -12,6 +12,14 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactRouter = require("react-router");
 
+var _RoomItem = require("./RoomItem");
+
+var _RoomItem2 = _interopRequireDefault(_RoomItem);
+
+var _firebaseBrowser = require("firebase/firebase-browser");
+
+var _firebaseBrowser2 = _interopRequireDefault(_firebaseBrowser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -20,47 +28,159 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var ICON_CHAT_STYLE = {
+  fontSize: 120,
+  color: "#DDD"
+};
+
+var FORM_STYLE = {
+  display: "flex"
+};
+
+var BUTTON_STYLE = {
+  marginLeft: 10
+};
+
 var Rooms = function (_React$Component) {
   _inherits(Rooms, _React$Component);
 
-  function Rooms() {
+  function Rooms(props) {
     _classCallCheck(this, Rooms);
 
-    return _possibleConstructorReturn(this, (Rooms.__proto__ || Object.getPrototypeOf(Rooms)).apply(this, arguments));
+    var _this = _possibleConstructorReturn(this, (Rooms.__proto__ || Object.getPrototypeOf(Rooms)).call(this, props));
+
+    _this.state = {
+      roomName: "",
+      rooms: []
+    };
+    _this.db = _firebaseBrowser2.default.database();
+    _this.handleOnChangeRoomName = _this.handleOnChangeRoomName.bind(_this);
+    _this.handleOnClickCreate = _this.handleOnClickCreate.bind(_this);
+    return _this;
   }
 
   _createClass(Rooms, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.fetchRooms();
+    }
+  }, {
+    key: "handleOnChangeRoomName",
+    value: function handleOnChangeRoomName(e) {
+      this.setState({
+        roomName: e.taget.value
+      });
+    }
+  }, {
+    key: "handleOnClickCreate",
+    value: function handleOnClickCreate(e) {
+      var _this2 = this;
+
+      var roomName = this.state.roomName;
+
+      e.preventDefault();
+      if (!roomName.length) {
+        return;
+      }
+
+      var newRoomRef = this.db.ref("/chatrooms").push();
+      var newRoom = {
+        description: roomName
+      };
+
+      newRoomRef.update(newRoom).then(function () {
+        _this2.setState({ roomName: "" });
+        return _this2.fetchRooms().then(function () {
+          _reactRouter.hashHistory.push("/rooms/" + newRoomRef.key);
+        });
+      });
+    }
+  }, {
+    key: "fetchRooms",
+    value: function fetchRooms() {
+      var _this3 = this;
+
+      return this.db.ref("/chatrooms").limitToLast(20).once("value").then(function (snapshot) {
+        var rooms = [];
+        snapshot.forEach(function (item) {
+          rooms.push(Object.assign({ key: item.key }, item, val()));
+        });
+        _this3.setState({ rooms: rooms });
+      });
+    }
+  }, {
+    key: "renderRoomList",
+    value: function renderRoomList() {
+      var roomId = this.props.params.roomId;
+      var _state = this.state,
+          rooms = _state.rooms,
+          roomName = _state.roomName;
+
+      return _react2.default.createElement(
+        "div",
+        { className: "list-group" },
+        rooms.map(function (r) {
+          return _react2.default.createElement(_RoomItem2.default, { room: r, key: r.key, selected: r.key === roomId });
+        }),
+        _react2.default.createElement(
+          "div",
+          { className: "list-group-header" },
+          _react2.default.createElement(
+            "form",
+            { style: FORM_STYLE, onSubmit: this.handleOnClickCreate },
+            _react2.default.createElement("input", {
+              type: "text",
+              className: "form-control",
+              placeholder: "New room",
+              onChange: this.handleOnChangeRoomName,
+              value: roomName
+            }),
+            _react2.default.createElement(
+              "button",
+              { className: "btn btn-default", style: BUTTON_STYLE },
+              _react2.default.createElement("span", { className: "icon icon-plus" })
+            )
+          )
+        )
+      );
+    }
+  }, {
+    key: "renderRoom",
+    value: function renderRoom() {
+      if (this.props.children) {
+        return this.props.children;
+      } else {
+        return _react2.default.createElement(
+          "div",
+          { className: "text-center" },
+          _react2.default.createElement(
+            "div",
+            { style: ICON_CHAT_STYLE },
+            _react2.default.createElement("span", { className: "icon icon-chat" })
+          ),
+          _react2.default.createElement(
+            "p",
+            null,
+            "Join a chat room from the sidebar or create your chat room."
+          )
+        );
+      }
+    }
+  }, {
     key: "render",
     value: function render() {
       return _react2.default.createElement(
         "div",
-        null,
+        { className: "pane-group" },
         _react2.default.createElement(
-          "h2",
-          null,
-          "Rooms"
+          "div",
+          { className: "pane-sm side-bar" },
+          this.renderRoomList()
         ),
         _react2.default.createElement(
-          "ul",
-          null,
-          _react2.default.createElement(
-            "li",
-            null,
-            _react2.default.createElement(
-              _reactRouter.Link,
-              { to: "/rooms/1" },
-              "Room 1"
-            )
-          ),
-          _react2.default.createElement(
-            "li",
-            null,
-            _react2.default.createElement(
-              _reactRouter.Link,
-              { to: "/rooms/2" },
-              "Room 2"
-            )
-          )
+          "div",
+          { className: "pane" },
+          this.renderRoom()
         )
       );
     }
