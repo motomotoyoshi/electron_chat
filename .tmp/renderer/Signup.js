0ef5419e05dede8a322d76fd30a2437a44eb7e8c
motomotoyoshi <motomoto.git@gmail.com>
2019-12-26 03:06:59 +0900

    Update Signup

--- .tmp/renderer/Signup.js
+++ .tmp/renderer/Signup.js
@@ -12,6 +12,14 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactRouter = require("react-router");
 
+var _Errors = require("./Errors");
+
+var _Errors2 = _interopRequireDefault(_Errors);
+
+var _firebaseBrowser = require("firebase/firebase-browser");
+
+var _firebaseBrowser2 = _interopRequireDefault(_firebaseBrowser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -20,38 +28,186 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
+var SIGNUP_FORM_STYLE = {
+  margin: "0 auto",
+  padding: 30
+};
+
+var CANCEL_BUTTON_STYLE = {
+  marginLeft: 10
+};
+
 var Signup = function (_React$Component) {
   _inherits(Signup, _React$Component);
 
-  function Signup() {
+  function Signup(props) {
     _classCallCheck(this, Signup);
 
-    return _possibleConstructorReturn(this, (Signup.__proto__ || Object.getPrototypeOf(Signup)).apply(this, arguments));
+    var _this = _possibleConstructorReturn(this, (Signup.__proto__ || Object.getPrototypeOf(Signup)).call(this, props));
+
+    _this.state = {
+      email: "",
+      password: "",
+      name: "",
+      photoURL: "",
+      errors: []
+    };
+
+    _this.handleOnChangeEmail = _this.handleOnChangeEmail.bind(_this);
+    _this.handleOnChangePassword = _this.handleOnChangePassword.bind(_this);
+    _this.handleOnChangeName = _this.handleOnChangeName.bind(_this);
+    _this.handleOnChangePhotoURL = _this.handleOnChangePhotoURL.bind(_this);
+    _this.handleOnSubmit = _this.handleOnSubmit.bind(_this);
+    return _this;
   }
 
   _createClass(Signup, [{
+    key: "handleOnChangeEmail",
+    value: function handleOnChangeEmail(e) {
+      this.setState({ email: e.target.value });
+    }
+  }, {
+    key: "handleOnChangePassword",
+    value: function handleOnChangePassword(e) {
+      this.setState({ password: e.target.value });
+    }
+  }, {
+    key: "handleOnChangeName",
+    value: function handleOnChangeName(e) {
+      this.setState({ name: e.target.value });
+    }
+  }, {
+    key: "handleOnChangePhotoURL",
+    value: function handleOnChangePhotoURL(e) {
+      this.setState({ photoURL: e.target.value });
+    }
+  }, {
+    key: "handleOnSubmit",
+    value: function handleOnSubmit(e) {
+      var _this2 = this;
+
+      var _state = this.state,
+          email = _state.email,
+          password = _state.password,
+          photoURL = _state.photoURL;
+
+      var errors = [];
+      var isValid = true;
+      e.preventDefault();
+
+      if (!email.length) {
+        isValid = false;
+        errors.push("Email can't be blank.");
+      }
+      if (!password.length) {
+        isValid = false;
+        errors.push("password can't be blank.");
+      }
+      if (!isValid) {
+        this.setState({ errors: errors });
+        return;
+      }
+
+      _firebaseBrowser2.default.auth().createUserWithEmailAndPassword(email, password).then(function (newUser) {
+        return newUser.updateProfile({
+          displayName: name,
+          photoURL: photoURL
+        });
+      }).then(function () {
+        _reactRouter.hashHistory.push("/rooms");
+      }).catch(function () {
+        _this2.setState({ errors: [err.message] });
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
       return _react2.default.createElement(
-        "div",
-        null,
+        "form",
+        { style: SIGNUP_FORM_STYLE, onSubmit: this.handleOnSubmit },
+        _react2.default.createElement(_Errors2.default, { errorMessages: this.state.errors }),
         _react2.default.createElement(
-          "h2",
-          null,
-          "Signup"
+          "div",
+          { className: "form-group" },
+          _react2.default.createElement(
+            "label",
+            null,
+            "Email*"
+          ),
+          _react2.default.createElement("input", {
+            type: "email",
+            className: "form-control",
+            placeholder: "email",
+            onChange: this.handleOnChangeEmail,
+            value: this.state.email
+          })
         ),
         _react2.default.createElement(
-          _reactRouter.Link,
-          { to: "/rooms" },
-          "Create new account"
+          "div",
+          { className: "form-group" },
+          _react2.default.createElement(
+            "label",
+            null,
+            "Password*"
+          ),
+          _react2.default.createElement("input", {
+            type: "password",
+            className: "form-control",
+            placeholder: "password",
+            onChange: this.handleOnChangePassword,
+            value: this.state.password
+          })
         ),
-        _react2.default.createElement("br", null),
         _react2.default.createElement(
-          _reactRouter.Link,
-          { to: "/login" },
-          "cancel"
+          "div",
+          { className: "form-group" },
+          _react2.default.createElement(
+            "label",
+            null,
+            "User name*"
+          ),
+          _react2.default.createElement("input", {
+            type: "text",
+            className: "form-control",
+            placeholder: "user name",
+            onChange: this.handleOnChangeName,
+            value: this.state.name
+          })
         ),
-        _react2.default.createElement("br", null)
+        _react2.default.createElement(
+          "div",
+          { className: "form-group" },
+          _react2.default.createElement(
+            "label",
+            null,
+            "Photo URL"
+          ),
+          _react2.default.createElement("input", {
+            type: "text",
+            className: "form-control",
+            placeholder: "photo URL",
+            onChange: this.handleOnChangePhotoURL,
+            value: this.state.photoURL
+          })
+        ),
+        _react2.default.createElement(
+          "div",
+          { className: "form-group" },
+          _react2.default.createElement(
+            "button",
+            { className: "btn btn-large btn-primary" },
+            "Create new account"
+          ),
+          _react2.default.createElement(
+            _reactRouter.Link,
+            { to: "/login" },
+            _react2.default.createElement(
+              "button",
+              { type: "button", style: CANCEL_BUTTON_STYLE, className: "btn btn-large btn-default" },
+              "Cancel"
+            )
+          )
+        )
       );
     }
   }]);
--- src/renderer/Signup.jsx
+++ src/renderer/Signup.jsx
@@ -1,14 +1,132 @@
 import React from "react";
-import { Link } from "react-router";
+import { Link, hashHistory  } from "react-router";
+import Errors from "./Errors";
+import firebase from "firebase/firebase-browser";
+
+const SIGNUP_FORM_STYLE = {
+  margin: "0 auto",
+  padding: 30
+};
+
+const CANCEL_BUTTON_STYLE = {
+  marginLeft: 10
+};
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      name: "",
+      photoURL: "",
+      errors: []
+    };
+
+    this.handleOnChangeEmail = this.handleOnChangeEmail.bind(this);
+    this.handleOnChangePassword = this.handleOnChangePassword.bind(this);
+    this.handleOnChangeName = this.handleOnChangeName.bind(this);
+    this.handleOnChangePhotoURL = this.handleOnChangePhotoURL.bind(this);
+    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+  }
+
+  handleOnChangeEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+  handleOnChangePassword(e) {
+    this.setState({ password: e.target.value });
+  }
+  handleOnChangeName(e) {
+    this.setState({ name: e.target.value });
+  }
+  handleOnChangePhotoURL(e) {
+    this.setState({ photoURL: e.target.value });
+  }
+  
+  handleOnSubmit(e) {
+    const { email, password, photoURL } = this.state;
+    const errors = [];
+    let isValid = true;
+    e.preventDefault();
+
+    if (!email.length) {
+      isValid = false;
+      errors.push("Email can't be blank.")
+    }
+    if (!password.length) {
+      isValid = false;
+      errors.push("password can't be blank.")
+    }
+    if (!isValid) {
+      this.setState({ errors });
+      return;
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(email, password).then((newUser) => {
+      return newUser.updateProfile({
+        displayName: name,
+        photoURL
+      });
+    }).then(() => {
+      hashHistory.push("/rooms");
+    }).catch(() => {
+      this.setState({ errors: [err.message] });
+    });
+    
+  }
+  
+
   render() {
     return(
-      <div>
-        <h2>Signup</h2>
-        <Link to="/rooms">Create new account</Link><br/>
-        <Link to="/login">cancel</Link><br/>
-      </div>
+      <form style={SIGNUP_FORM_STYLE} onSubmit={this.handleOnSubmit}>
+        <Errors errorMessages={this.state.errors} />
+        <div className="form-group">
+          <label>Email*</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="email"
+            onChange={this.handleOnChangeEmail}
+            value={this.state.email}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password*</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="password"
+            onChange={this.handleOnChangePassword}
+            value={this.state.password}
+          />
+        </div>
+        <div className="form-group">
+          <label>User name*</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="user name"
+            onChange={this.handleOnChangeName}
+            value={this.state.name}
+          />
+        </div>
+        <div className="form-group">
+          <label>Photo URL</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="photo URL"
+            onChange={this.handleOnChangePhotoURL}
+            value={this.state.photoURL}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-large btn-primary">Create new account</button>
+            <Link to="/login">
+              <button type="button" style={CANCEL_BUTTON_STYLE} className="btn btn-large btn-default">Cancel</button>
+            </Link>
+          </div>
+      </form>
     );
   }
 }
